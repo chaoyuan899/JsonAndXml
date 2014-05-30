8b0dfd21080eb379e1a39127d8a23382176ed2c6
chaoyuan899 <chaoyuan899@gmail.com>
2014-05-30 11:40:50 +0800

    Initial Commit

---
+++ JsonAndXml/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JsonAndXml
+//
+//  Created by aaron on 14-5-30.
+//  Copyright (c) 2014年 The Technology Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JsonAndXml/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  JsonAndXml
+//
+//  Created by aaron on 14-5-30.
+//  Copyright (c) 2014年 The Technology Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
